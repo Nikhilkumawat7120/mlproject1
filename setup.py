1ab3687a3bf70e4699e4ffda666b6dc267fd29d9
Nikhil <nikhilkumarkumawat71200@gmail.com>
2025-12-19 19:46:23 +0530

    project Structer

--- setup.py
+++ setup.py
@@ -1,17 +1,23 @@
 from setuptools import find_packages,setup
-from typing import list
+from typing import List
 
-def get_requirements(file_path:str)->List[str]
-'''
-this function return the list of requirements
-'''
-requirements=[]
 
-with open(file_path) as file_obj:
-    requirements=file_obj.readlines()
-    requirements=[req.replace('/n',"")for req in requirements]
 
-return requirements
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path: str) -> List[str]:
+
+    '''
+    this function return the list of requirements
+    '''
+    requirements=[]
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('/n',"")for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    return requirements
 
 
 
@@ -24,5 +30,5 @@ version='0.0.1',
 author='nikhil',
 author_email='nihkilkumarkumawat7120@gmail.com', 
 packages=find_packages(),
-install_requires=get_requirements(requirements.txt),
+install_requires=get_requirements("requirements.txt"),
 )
---
+++ template.py
@@ -0,0 +1,50 @@
+import os  #gives generic folder path
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+project_name="mlproject"
+
+
+list_of_files=[
+
+    ".gthub/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/components/data_ingestion.py",
+    f"src/{project_name}/components/data_transformation",
+    f"src/{project_name}/components/model_trainer.py",
+    f"src/{project_name}/components/model_monitering.py",
+
+     f"src/{project_name}/pipelines/__init__.py",
+     f"src/{project_name}/pipelines/traning_pipeline.py",
+     f"src/{project_name}/pipelines/prediction_pipeline.py",
+     f"src/{project_name}/exception.py",
+     f"src/{project_name}/utils.py"
+     "app.py"
+     "Dockerfle"
+     "requirements.txt"
+     "setup.py"
+
+
+
+
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir,filename=os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
+
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,'w') as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exists")
